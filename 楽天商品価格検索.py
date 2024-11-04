b74b96dddd25b9b09df175fa666ebba3c6168e68
CoffeeTonya <127587138+CoffeeTonya@users.noreply.github.com>
2024-11-04 17:10:59 +0900

    Update 楽天商品価格検索.py

--- 楽天商品価格検索.py
+++ 楽天商品価格検索.py
@@ -171,9 +171,9 @@ if selected_item == 'csv検索':
 
     # 表形式で出力
     for i in range(len(price_data["粗利率"])):
-        st.write(f"**粗利率**: {price_data['粗利率'][i]}")
-        st.write(f"**価格設定**: {price_data['価格設定'][i]}")
-        st.markdown("---")  # 区切り線
+        st.write(price_data['粗利率'][i])
+        st.write(price_data['価格設定'][i])
+        st.markdown("---") 
 
     st.sidebar.markdown('csv1: リスト内商品すべて検索<br>csv1&2: 販売中のみ検索<br>csv2: 検索不可', unsafe_allow_html=True)
     st.sidebar.markdown("* * * ")
